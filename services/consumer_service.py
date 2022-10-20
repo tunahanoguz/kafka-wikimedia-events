0780c984ceab0092a4a9dfd93510e54aeddac4f1
tunahanoguz <tunahanog@gmail.com>
2022-10-20 07:12:12 +0300

    Add logging to consumer service

--- services/consumer_service.py
+++ services/consumer_service.py
@@ -2,23 +2,34 @@ from kafka.consumer.fetcher import ConsumerRecord
 from context_managers import ConsumerContextManager
 from concurrent.futures import ThreadPoolExecutor
 from models import WikimediaEvent
+from services.logging_service import LoggingService
+from exceptions import EventDataParsingException
 import json
 
 
 class ConsumerService:
     def __init__(self, consumer_context_manager: object):
         self._consumer_cxm = consumer_context_manager
+        logging_service = LoggingService('producer')
+        self._logging = logging_service.create_logging()
 
     def consume_message(self):
-        with self._consumer_cxm as p:
-            for message in p:
-                print(self._parse_message(message))
-                p.commit()
+        try:
+            with self._consumer_cxm as p:
+                for message in p:
+                    print(self._parse_message(message))
+                    p.commit()
+        except Exception as e:
+            self._logging.exception(e)
+            pass
 
     @staticmethod
     def _parse_message(message: ConsumerRecord) -> WikimediaEvent:
-        message_json = json.loads(message.value.decode('utf8').replace("'", '"'))
-        return WikimediaEvent(**message_json)
+        try:
+            message_json = json.loads(message.value.decode('utf8').replace("'", '"'))
+            return WikimediaEvent(**message_json)
+        except Exception as e:
+            raise EventDataParsingException(e)
 
 
 if __name__ == '__main__':
